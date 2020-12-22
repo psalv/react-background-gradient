d03c2800fc922a4dd4b70a9ac362fe83f2ed8502
Paul Salvatore <paul@faire.com>
2020-12-22 12:07:30 -0500

    Fix config

--- rollup.config.js
+++ rollup.config.js
@@ -26,7 +26,7 @@ export default {
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
+      exclude: ["**/__tests__/**", "**/__stories__/**"],
       clean: true,
     }),
     commonjs({
